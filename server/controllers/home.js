769fdb3da1f461c455aa54086ddb0d56d86e8568
jcintron82 <71289948+jcintron82@users.noreply.github.com>
2023-02-03 19:21:42 -0500

    made api call async

--- server/controllers/home.js
+++ server/controllers/home.js
@@ -2,16 +2,17 @@ const mongoose = require('mongoose');
 const items = require("../DBschemas/items");
 
 const arr = [];
+const x = arr.toString();
+
 module.exports = {
-  
   getIndex: (req, res) => {
     const dbQuery = items.find({}, (err, cursor) => {
       cursor.forEach((dataset) => {
-        arr.push(dataset)
+        arr.push(dataset);
       });
-      console.log(arr)
+      console.log(arr);
     })
 
-    // res.json({message:dbQuery})
+    res.send({message:arr})
   }
 };
