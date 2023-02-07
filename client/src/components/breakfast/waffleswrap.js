e044f29f15c69ca18ffafc1773294f5f81d2abe3
jcintron82 <71289948+jcintron82@users.noreply.github.com>
2023-02-07 16:26:38 -0500

    Solved bug causing the first server response to the client to be undefined as well as causing subsequent responses to be incorrect occasionally

--- client/src/components/breakfast/waffleswrap.js
+++ client/src/components/breakfast/waffleswrap.js
@@ -1,60 +1,65 @@
-import { useState, useEffect } from 'react';
-
-import { OrderPad } from '../orderpad.js'
-import { UtilityBar } from '../utilitybar'
+import { OrderPad } from "../orderpad.js";
+import { UtilityBar } from "../utilitybar";
 import "../../css/mainscreen/homescreen-styles.css";
+
 // const mongoose = require("mongoose");
 export const WafflesScreen = () => {
-    const [message, setMessage] = useState("");
-    const [productState, setProduct] = useState('Plain');
-    let arr = [];
+  let queryArr = [];
 
-const queryProduct = (input) => {
-    arr.push(input);
+  const queryProduct = async (input) => {
+    queryArr.push(input);
     try {
-    // Send data to the backend via POST
-     fetch('http://localhost:8000', {
-
-method: 'POST', 
-headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(arr)
-})
+      // Send data to the backend via POST
+      const pull = await fetch("http://localhost:8000", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(arr),
+      });
+      queryArr.splice(0);
+      addToOrderClick();
+    } catch (err) {
+      console.log(err);
     }
-    catch(err){
-        console.log('Error Here!')
-        console.log(err);
-    }
-addToOrderClick();
-arr.splice(0);
-}
+  };
 
-const addToOrderClick = async () => {
+  const addToOrderClick = async () => {
     try {
-    const call = await fetch("http://localhost:8000");
-    const data = await call.json();
-    
-        setMessage(data);
+      const call = await fetch("http://localhost:8000");
+      const data = await call.json();
+      console.log(data);
+    } catch (err) {
+      console.log(err);
     }
-    catch(err){
-        console.log(err)
-    }
-
-}
+  };
 
   return (
-    <div className='body'>
-    <div className="mainwrap">
-      <OrderPad />
-      <div className="categorybtnswrap">
-            <button type='submit' onClick={(e) => {queryProduct('Plain Waffle')}} className="categorybtns">Plain
-            </button>
-            <button type='submit' onClick={(e) => {queryProduct('Blueberry Waffle')}}  className="categorybtns">Blueberry
-            </button>
-            </div>
-    </div>
+    <div className="body">
+      <div className="mainwrap">
+        <OrderPad />
+        <div className="categorybtnswrap">
+          <button
+            type="submit"
+            onClick={(e) => {
+              queryProduct("Plain Waffle");
+            }}
+            className="categorybtns"
+          >
+            Plain
+          </button>
+          <button
+            type="submit"
+            onClick={(e) => {
+              queryProduct("Blueberry Waffle");
+            }}
+            className="categorybtns"
+          >
+            Blueberry
+          </button>
+        </div>
+      </div>
       <UtilityBar />
-  </div>
+    </div>
   );
-}
+};
 
 export default WafflesScreen;
