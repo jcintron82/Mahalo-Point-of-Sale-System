d9c5b7200a2678e06f1d15070125b366a535a569
jcintron82 <71289948+jcintron82@users.noreply.github.com>
2023-02-27 15:53:00 -0500

    fixed typo

--- server/server.js
+++ server/server.js
@@ -112,7 +112,7 @@ app.use("/", mainRoutes);
 
 //Server Running
 if (process.env.PORT) {
-  pp.listen(process.env.PORT, () => {
+  app.listen(process.env.PORT, () => {
     // console.log("Server is running on port" + process.env.PORT);
   });
 }
